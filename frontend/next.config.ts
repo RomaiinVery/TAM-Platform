60e39f660dc57aa08601ce87efe2bfc2b72b68fb
vschellervidal <valentin.scheller-vidal@epitech.eu>
2025-10-02 12:52:16 +0200

    chore(frontend): turbopack -> force la racine sur frontend/

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: __dirname, // ← force la racine sur “frontend/”
+  },
 };
 
 export default nextConfig;
